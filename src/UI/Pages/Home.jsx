4e53ec258b3e8fcbf206d98f139f5b8a40c48637
Nicolas Soler <jnsoler04@gmail.com>
2024-01-05 16:22:31 -0500

    Updates

---
+++ src/UI/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image1 from 'Utilities/Image1.png';
+
+export default function () {
+    return (
+        <div>Abecedario
+            <img src={Image1} alt="" />
+        </div>
+    )
+}
